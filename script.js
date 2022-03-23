491d5a368c16e3b661b1fbd9afd1fcf4186e12c8
Jack-19 <total90-@hotmail.com>
2022-03-23 19:22:41 +0000

    7th

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+var work = documnet.getElementById("offboth")
+var off;
+
+
+
+if( work === jack){
+    off = "Good day";
+} else {
+  off = "Good evening";
+}
+
+
+document.getElementById("demo").innerHTML = off;
